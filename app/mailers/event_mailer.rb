4a89c00cd5f7428e62dffc87bccaa4c7ae95e864
marunbai <marunbai@gmail.com>
2014-07-30 23:31:33 -0400

    email update

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -6,7 +6,7 @@ class EventMailer < ActionMailer::Base
     @user = user
     @event = event
     subject = "Cancellation: Rescuing Leftover Cuisine at #{event.title} on #{event.starting_time.strftime('%B %e')}"
-    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/RLC_LOGO.png")
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/RLC_LOGO_small.png")
     mail(to: user.email, subject: subject)
   end
 end
